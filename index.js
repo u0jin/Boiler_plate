503fba3a60afa3469909dace6efd242fdbd0a6df
ujinee <yujin5836@gmail.com>
2021-01-05 22:53:53 +0900

    Update : Connect mongoDB + install mongoose
    connecet mongoDB & setting DBuser

--- index.js
+++ index.js
@@ -2,6 +2,15 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://root:****@boilerplate.6ur9n.mongodb.net/<dbname>?retryWrites=true&w=majority',{
+    useNewUrlParser: true, useUnifiedTopology: true,
+    useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err))
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
