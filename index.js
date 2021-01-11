c6ebc1c6a3fa3c67fe44c74d88ef3c292f96bb95
ujinee <yujin5836@gmail.com>
2021-01-12 00:20:54 +0900

    Update : login
    update login & model.user

--- index.js
+++ index.js
@@ -40,6 +40,27 @@ app.post('/register', (req, res) => {
 })
 
 
+app.post('/login', (req, res) => {
+  User.findOne({ email: req.body.email}, (err,userInfo) =>{
+    if(!userInfo){
+      return res.json({
+        loginSuccess : false,
+        message : "제공된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+
+    user.comparePassword(req.body.password , (err, isMatch)=>{
+      if(!isMatch)
+      return res.json({loginSuccess : false, message:"비밀번호가 틀렸습니다."})
+
+      user.generateToken((err,user) => {
+
+      })
+    
+    
+    })
+  })
+})
 
 
 app.listen(port, () => {
--- models/User.js
+++ models/User.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
+
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+
 const jwt = require('jsonwebtoken');
 const moment = require("moment");
+
 
 const userSchema = mongoose.Schema({
     name: {
@@ -34,6 +37,33 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+userSchema.pre('save', function( next ) {
+    var user = this;
+    
+    if(user.isModified('password')){    
+        // console.log('password changed')
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if(err) return next(err);
+    
+            bcrypt.hash(user.password, salt, function(err, hash){
+                if(err) return next(err);
+                user.password = hash 
+                next()
+            })
+        })
+    } else {
+        next()
+    }
+});
+
+userSchema.methods.comparePassword = function(plainPassword,cb){
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if (err) return cb(err);
+        cb(null, isMatch)
+    })
+}
+
 
 
 const User = mongoose.model('User', userSchema);
